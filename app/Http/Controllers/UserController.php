d7ca69316a4ab3686d0abbde0bd9e2b6f9673a85
seunbambo <seun.bambo@gmail.com>
2020-09-08 02:21:20 +0100

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symphony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    public function index() {
+        return user::all();
+    }
+
+    public function show($id) {
+        return User::find($id);
+    }
+
+    public function store(Request $request) {
+        $user = User::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response($user, 201);
+    }
+
+    public function update(Request $request, $id) {
+        $user = User::find($id);
+
+        $user->update([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response($user, 202);
+    }
+}
