549db843d5222f5486f327bd5f151d971d241d40
seunbambo <seun.bambo@gmail.com>
2020-09-17 03:44:40 +0100

    Install passport dependency

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Http\Requests\UserCreateRequest;
+use App\Http\Requests\UserUpdateRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Symphony\Component\HttpFoundation\Response;
@@ -10,34 +12,26 @@ use Symphony\Component\HttpFoundation\Response;
 class UserController extends Controller
 {
     public function index() {
-        return user::all();
+        return User::paginate();
     }
 
     public function show($id) {
         return User::find($id);
     }
 
-    public function store(Request $request) {
-        $user = User::create([
-            'first_name' => $request->input('first_name'),
-            'last_name' => $request->input('last_name'),
-            'email' => $request->input('email'),
-            'password' => Hash::make($request->input('password')),
+    public function store(UserCreateRequest $request) {
+        $user = User::create($request->only('first_name', 'last_name', 'email') + [
+            'password' => Hash::make(1234),
         ]);
 
         return response($user, 201);
     }
 
-    public function update(Request $request, $id) {
+    public function update(UserUpdateRequest $request, $id) {
         $user = User::find($id);
 
-        $user->update([
-            'first_name' => $request->input('first_name'),
-            'last_name' => $request->input('last_name'),
-            'email' => $request->input('email'),
-            'password' => Hash::make($request->input('password')),
-        ]);
-
+        $user->update($request->only('first_name', 'last_name', 'email'));
+        
         return response($user, 202);
     }
 
