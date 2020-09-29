46be9b038ba5229d3d34fcf545b16aa3f9da5a5f
seunbambo <seun.bambo@gmail.com>
2020-09-29 22:18:30 +0100

    Local commit

--- app/User.php
+++ app/User.php
@@ -34,6 +34,8 @@ use Laravel\Passport\HasApiTokens;
  * @property-read int|null $clients_count
  * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Passport\Token[] $tokens
  * @property-read int|null $tokens_count
+ * @property int $role_id
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRoleId($value)
  */
 class User extends Authenticatable
 {
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,8 +19,8 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'first_name' => $faker->name,
-        'last_name' => $faker->name,
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'role_id' => \App\Role::InRandomOrder()->first()->id
