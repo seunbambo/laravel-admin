f9384d02fd846edc1a078cd5009d8eab523745df
Oluwaseun Adebambo <emma.bambo@yahoo.com>
2020-09-30 13:21:39 +0100

    Add chart and dashboard APIs

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,7 @@ Route::post('register', 'AuthController@register');
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('user', 'UserController@user');
+    Route::get('chart', 'DashboardController@chart');
     Route::put('users/info', 'UserController@updateInfo');
     Route::put('users/password', 'UserController@updatePassword');
     Route::post('upload', 'ImageController@upload');
