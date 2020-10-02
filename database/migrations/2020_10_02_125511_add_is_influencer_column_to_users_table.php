7c99b6efff90ca62ebd17e924c2dce21023b77bf
Oluwaseun Adebambo <emma.bambo@yahoo.com>
2020-10-02 17:28:26 +0100

    influencers column

--- database/migrations/2020_10_02_125511_add_is_influencer_column_to_users_table.php
+++ database/migrations/2020_10_02_125511_add_is_influencer_column_to_users_table.php
@@ -22,7 +22,7 @@ class AddIsInfluencerColumnToUsersTable extends Migration
      * Reverse the migrations.
      *
      * @return void
-     */ 
+     */
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
