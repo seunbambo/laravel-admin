24eba2fe802ecdf10c60a10ea1000ff75fc8c308
Oluwaseun Adebambo <emma.bambo@yahoo.com>
2020-11-16 11:59:46 +0100

    Checkout links

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -19,9 +19,9 @@ class UserResource extends JsonResource
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'email' => $this->email,
-            $this->mergeWhen(\Auth::user()->isAdmin(), [
+            $this->mergeWhen(\Auth::user() && \Auth::user()->isAdmin(), [
                 'role' => $this->role
-            ]),
+            ])
         ];
     }
 }
